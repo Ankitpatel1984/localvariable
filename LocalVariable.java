1139495ea496bed4170a60a004df8ad33c6f1206
Ankit Patel <ankitpatel1984@gmail.com>
2023-10-30 17:22:54 +0000

    localvariable

---
+++ LocalVariable.java
@@ -0,0 +1,23 @@
+package localvariable;
+
+public class LocalVariable {
+
+    void method1() {
+
+        int a = 10;
+    }
+
+    void method2() {
+
+        int b = 10;
+    }
+
+    void method3() {
+        int c = 20;
+    }
+
+    public static void main(String[] args) {
+
+    }
+    }
+
